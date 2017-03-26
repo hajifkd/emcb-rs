be9ac63925d41fceeb46b31b11989d583f39b970
Hajime Fukuda <hajime.fukuda@me.com>
2017-03-26 23:43:24 +0900

    succeed to register callback

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+extern crate emval_sys;
+
+use emval_sys::*;
+
 fn main() {
-    println!("Hello, world!");
+    unsafe {
+        static VOID_ID: &'static str = "rust_void\0";
+        _embind_register_void(VOID_ID.as_ptr() as _,
+                              VOID_ID.as_ptr() as _);
+
+        static FUNC_ID: &'static str = "rust_func\0";
+
+        static mut ARGS_LIST: [*const usize; 1] = [0 as _; 1];
+
+        let cbfn = || {
+            println!("Hello, callback in native Rust!");
+        };
+
+        let cbptr = Box::into_raw(Box::new(&cbfn as &Fn()));
+
+        ARGS_LIST[0] = VOID_ID.as_ptr() as _;
+
+        extern fn hoge(arg1: isize) {
+            println!("Hello, involer!");
+            let cb: Box<&Fn()> = unsafe { Box::from_raw(arg1 as _) };
+            cb();
+        }
+
+        _embind_register_function(FUNC_ID.as_ptr() as _,
+                                  1,
+                                  //(VOID_ID.as_ptr()) as _,
+                                  ARGS_LIST.as_ptr() as _,
+                                  "vi\0".as_ptr() as _,
+                                  hoge as _,
+                                  cbptr as _);
+    }
 }
